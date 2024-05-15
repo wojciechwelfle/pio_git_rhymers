123612bc90903b25894d5331addacba550fa0d9e
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-05-15 20:55:57 +0200

    9. zmiana widocznosci atrybutów klas

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -3,8 +3,8 @@ package edu.kis.vh.nursery.list;
 public class IntLinkedList {
 
     private static final int EMPTY_VALUE = -1;
-    Node last;
-    int i;
+    private Node last;
+    private int i;
 
     public void push(int i) {
         if (last == null)
--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,8 +2,8 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    public final int value;
-    public Node prev, next;
+    protected final int value;
+    protected Node prev, next;
 
     public Node(int i) {
         value = i;
