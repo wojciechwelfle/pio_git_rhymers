e8b771b62190366cbf448cdf81704474e4c3d764
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-05-15 20:57:33 +0200

    10. getter i setter dla pola total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -34,4 +34,12 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
 }
