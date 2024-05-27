fd550482e4c559722672129555691919235057d4
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-05-27 13:16:45 +0200

    15. walidacja testów, wszystkie testy przeszły

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+    // All tests done correctly
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
