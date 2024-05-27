ce462dd7b917cf147fd51b87c5bc399d8def35d4
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2024-05-27 16:49:14 +0200

    17. wygenerowanie dokumentacji dla DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * The type DefaultCountingOutRhymer represents simple stack
+ */
 public class DefaultCountingOutRhymer {
 
     private static final int MAX_SIZE = 12;
@@ -9,25 +12,50 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY_VALUE;
 
+    /**
+     * Adds a number to the stack if it is not full.
+     *
+     * @param in - number to be added
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Checks if the stack is empty.
+     *
+     * @return true if the stack is empty, false otherwise
+     */
     public boolean callCheck() {
         return total == EMPTY_VALUE;
     }
 
+    /**
+     * Checks if the stack is full.
+     *
+     * @return true if the stack is full, false otherwise
+     */
     public boolean isFull() {
         return total == MAX_SIZE - 1;
     }
 
+    /**
+     * Returns top value of the stack.
+     *
+     * @return value at the top of stack - if stack is empty returns EMPTY_VALUE
+     */
     protected int peekaboo() {
         if (callCheck())
             return EMPTY_VALUE;
         return numbers[total];
     }
 
+    /**
+     * Removes and returns the value at the top of the stack.
+     *
+     * @return value at the top of stack - if stack is empty returns EMPTY_VALUE
+     */
     public int countOut() {
         if (callCheck())
             return EMPTY_VALUE;
